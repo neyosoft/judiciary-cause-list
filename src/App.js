cf9a2467541d84b4ca72206c74c6595ecfec534e
Obagunwa Emmanuel <access2emma@gmail.com>
2021-10-18 14:27:57 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaView, ScrollView, StatusBar, StyleSheet, useColorScheme} from 'react-native';
+
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+    const isDarkMode = useColorScheme() === 'dark';
+
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
+
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+            <NavigationContainer>
+                <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
+                    <Header />
+                </ScrollView>
+            </NavigationContainer>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+    },
+    highlight: {
+        fontWeight: '700',
+    },
+});
+
+export default App;
